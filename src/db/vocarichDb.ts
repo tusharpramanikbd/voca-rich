7bf161fc89e6a84db92b74964087e0695b7b78ac
Tushar Pramanik <tusharpramanikbd@gmail.com>
2026-01-24 21:52:04 +0600

    added db schema

---
+++ src/db/vocarichDb.ts
@@ -0,0 +1,31 @@
+import { Dexie, type EntityTable } from "dexie";
+
+interface Module {
+  id: string;
+  name: string;
+  createdAt: number;
+  updatedAt: number;
+}
+
+interface Word {
+  id: string;
+  moduleId: string;
+  word: string;
+  meaning: string;
+  createdAt: number;
+  updatedAt: number;
+}
+
+const db = new Dexie("VocaRich") as Dexie & {
+  modules: EntityTable<Module, "id">;
+  words: EntityTable<Word, "id">;
+};
+
+// Schema declaration:
+db.version(1).stores({
+  modules: "id, name, createdAt, updatedAt",
+  words: "id, moduleId, word, createdAt, updatedAt, [moduleId+word]",
+});
+
+export type { Module, Word };
+export { db };
