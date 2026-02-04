4ab15cc29cb27bb1c9f180e5aaa38debd8b32b3a
Tushar Pramanik <tusharpramanikbd@gmail.com>
2026-02-04 15:24:39 +0600

    fixed build issue

--- src/types/pwa.d.ts
+++ src/types/pwa.d.ts
@@ -1,13 +1,12 @@
-interface BeforeInstallPromptEvent extends Event {
-  readonly platforms: string[];
-  readonly userChoice: Promise<{
-    outcome: "accepted" | "dismissed";
-    platform: string;
-  }>;
-  prompt(): Promise<void>;
-}
-
 declare global {
+  interface BeforeInstallPromptEvent extends Event {
+    readonly platforms: string[];
+    readonly userChoice: Promise<{
+      outcome: "accepted" | "dismissed";
+      platform: string;
+    }>;
+    prompt(): Promise<void>;
+  }
   interface WindowEventMap {
     beforeinstallprompt: BeforeInstallPromptEvent;
   }
