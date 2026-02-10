6dd4af6e5ff2346f483f40651ba21ec3970839ac
Tushar Pramanik <tusharpramanikbd@gmail.com>
2026-02-10 20:31:51 +0600

    improved bottom modal sheet

--- src/components/Common/BottomSheet/BaseBottomSheet.tsx
+++ src/components/Common/BottomSheet/BaseBottomSheet.tsx
@@ -1,4 +1,4 @@
-import { AnimatePresence, motion } from "framer-motion";
+import { Sheet } from "react-modal-sheet";
 
 type TBaseBottomSheet = {
   isOpen: boolean;
@@ -6,30 +6,16 @@ type TBaseBottomSheet = {
   children: React.ReactNode;
 };
 
-const BaseBottomSheet = ({ isOpen, onClose, children }: TBaseBottomSheet) => (
-  <AnimatePresence>
-    {isOpen && (
-      <motion.div
-        initial={{ y: "100%" }}
-        animate={{ y: 0 }}
-        exit={{ y: "100%" }}
-        transition={{ type: "spring", damping: 40, stiffness: 300 }}
-        className="fixed inset-0 z-50 flex flex-col h-full"
-      >
-        <div
-          className="flex-1 bg-black/20 backdrop-blur-sm"
-          onClick={onClose}
-        />
-        <div className="bg-white/95 backdrop-blur-md rounded-t-3xl shadow-2xl border-t border-gray-100">
-          <div className="pt-4 pb-2 px-6 flex justify-center">
-            <div className="w-8 h-1.5 bg-gray-300 rounded-full" />
-          </div>
-
-          {children}
-        </div>
-      </motion.div>
-    )}
-  </AnimatePresence>
-);
+const BaseBottomSheet = ({ isOpen, onClose, children }: TBaseBottomSheet) => {
+  return (
+    <Sheet detent="content" isOpen={isOpen} onClose={onClose}>
+      <Sheet.Container>
+        <Sheet.Header />
+        <Sheet.Content>{children}</Sheet.Content>
+      </Sheet.Container>
+      <Sheet.Backdrop onClick={onClose} />
+    </Sheet>
+  );
+};
 
 export default BaseBottomSheet;
--- src/components/Words/AddWordFAB.tsx
+++ src/components/Words/AddWordFAB.tsx
@@ -1,5 +1,3 @@
-import { motion } from "framer-motion";
-
 type TAddWordFAB = {
   setIsAddSheetOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
@@ -7,13 +5,12 @@ type TAddWordFAB = {
 const AddWordFAB = ({ setIsAddSheetOpen }: TAddWordFAB) => {
   return (
     <div className="fixed bottom-6 right-6 z-40">
-      <motion.button
-        whileTap={{ scale: 0.95 }}
+      <button
         onClick={() => setIsAddSheetOpen(true)}
         className="w-16 h-16 bg-linear-to-r from-teal-500 to-blue-600 text-white rounded-2xl shadow-2xl hover:shadow-3xl flex items-center justify-center text-2xl"
       >
         +
-      </motion.button>
+      </button>
     </div>
   );
 };
