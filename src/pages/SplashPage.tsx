90f77e392ad389cf4cc9ede65ab2558a3ebb4135
Tushar Pramanik <tusharpramanikbd@gmail.com>
2026-02-10 19:43:12 +0600

    splash screen improved

--- src/pages/SplashPage.tsx
+++ src/pages/SplashPage.tsx
@@ -2,9 +2,9 @@ import AppLogo from "../assets/AppLogo";
 
 const SplashPage = () => {
   return (
-    <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-linear-to-br from-teal-500 via-blue-500 to-purple-600 px-6">
+    <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-linear-to-br from-teal-500 via-blue-500 to-purple-600 px-6 m-2 rounded-3xl shadow-lg">
       <div className="transition-all duration-300 opacity-100 scale-100">
-        <AppLogo className="w-48 h-20 mb-8 animate-pulse mx-auto" />
+        <AppLogo className="w-72 h-40 animate-pulse mx-auto" />
       </div>
     </div>
   );
