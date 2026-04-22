397eb4ae6e73406828333515b50c43113f602863
Tushar Pramanik <tusharpramanikbd@gmail.com>
2026-04-22 15:27:35 +0600

    add group sync icon and logic

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.m8f5hbic3h"
+    "revision": "0.ft4fcr31ngo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/Words/WordItem.tsx
+++ src/components/Words/WordItem.tsx
@@ -1,7 +1,12 @@
 import type { Word } from "../../db/vocarichDb";
 import { memo } from "react";
-import { EllipsisVerticalIcon } from "@heroicons/react/24/solid";
+import {
+  EllipsisVerticalIcon,
+  CheckCircleIcon,
+} from "@heroicons/react/24/solid";
 import { useWordsModal } from "../../providers/WordsModalProvider";
+import { useGroupsContext } from "../../providers/GroupsProvider";
+import { ALL_GROUP_ID } from "../../hooks/useGroups";
 
 type TWordItem = {
   word: Word;
@@ -9,6 +14,10 @@ type TWordItem = {
 
 const WordItem = ({ word }: TWordItem) => {
   const { openActions, openDetails } = useWordsModal();
+  const { selectedGroupId } = useGroupsContext();
+
+  const isAllGroup = selectedGroupId === ALL_GROUP_ID;
+  const isWordGrouped = !!word.groupId;
 
   return (
     <div
@@ -16,7 +25,14 @@ const WordItem = ({ word }: TWordItem) => {
       className="bg-white/70 backdrop-blur-md rounded-2xl p-3 shadow-lg border border-white/50 flex items-center justify-between"
     >
       <div className="flex-1">
-        <h3 className="text-2xl font-bold text-gray-900 mb-1">{word?.word}</h3>
+        <div className="flex items-center gap-1">
+          <h3 className="text-2xl font-bold text-gray-900 mb-1">
+            {word?.word}
+          </h3>
+          {isAllGroup && isWordGrouped && (
+            <CheckCircleIcon className="w-4 h-4" />
+          )}
+        </div>
         <p className="text-lg text-gray-700 leading-relaxed">{word?.meaning}</p>
       </div>
 
